8b1d561dc2229228eeb117fc686b1113deb755b9
zhangchen <zhangchen2397@126.com>
2014-10-09 00:24:59 +0800

    update by zc

--- slide.js
+++ slide.js
@@ -20,7 +20,11 @@
 
     var slide = function( config ) {
         this.defaultConfig = {
-
+            el: 'slide',
+            speed: 3000,
+            autoPlay: true,
+            continuous: true,
+            navClass: 'nav-list'
         };
 
         this.config = $.extend( this.defaultConfig, config || {} );
@@ -30,7 +34,52 @@
 
     $.extend( slide.prototype, {
         init: function() {
-            
+            this._cache();
+            this._createNav();
+            this._actContinue();
+            this._initEvent();
+        },
+
+        _cache: function() {
+            var me = this,
+                config = this.config;
+
+            this.el = ( typeof config.el === 'string' ) ? $( '#' + config.el ) : config.el;
+            this.imgUl = $( this.el.children()[ 0 ] );
+            this.imgList = this.imgUl.find( 'li' );
+            this.imgLen = this.imgList.length;
+        },
+
+        _createNav: function() {
+            var me = this,
+                config = this.config,
+                tempHtml = '<ul class="' + config.navClass + '">';
+
+            this.imgList.each( function( idx, el ) {
+                idx = idx + 1;
+                tempHtml += '<li>' + idx + '</li>';
+            } );
+
+            tempHtml += '</ul>';
+
+            this.el.append( tempHtml );
+
+            this.navUl = this.el.find( 'ul.' + config.navClass );
+            this.navList = this.navUl.find( 'li' );
+        },
+
+        _actContinue: function() {
+            var me = this,
+                config = this.config;
+
+            if ( config.continuous ) {
+                this.imgUl.append( $( this.imgList[ 0 ] ).clone() );
+                this.imgUl.append( $( this.imgList[ this.imgLen - 1 ] ).clone() );
+            }
+        },
+
+        _initEvent: function() {
+
         }
     } );
 
